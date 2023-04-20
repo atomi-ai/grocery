8bf46e6bb17176923c80675cbfcadcaacbaa0d96
dengquan-ihealth <quan.deng@ihealthlabs.com>
2023-04-20 19:51:11 +0800

    add test: Address provider operations

--- integration_test/firebase_integration_test.dart
+++ integration_test/firebase_integration_test.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fryo/src/entity/entities.dart';
+import 'package:fryo/src/provider/address_provider.dart';
 import 'package:fryo/src/provider/product_provider.dart';
 import 'package:fryo/src/provider/store_provider.dart';
 import 'package:fryo/src/provider/user_provider.dart';
@@ -119,5 +120,66 @@ void main() {
           expect(product.name, 'Hamburger');
           expect(product.price, 25.0);
         });
+  });
+
+  testWidgets('Address provider operations', (WidgetTester tester) async {
+    AddressProvider addressProvider = AddressProvider();
+
+    // 检查现在 address 列表
+    await addressProvider.fetchAddresses();
+    List<Address> initialAddresses = addressProvider.addresses;
+    int initialCount = initialAddresses.length;
+
+    // 添加一个 shipping address
+    Address shippingAddress = Address(
+      line1: '123 Shipping St.',
+      city: 'Shipping City',
+      state: 'Shipping State',
+      country: 'USA',
+      postalCode: '12345',
+    );
+    await addressProvider.addAddress(shippingAddress);
+
+    // 检查目前 address 列表是否增加一个 item
+    await addressProvider.fetchAddresses();
+    List<Address> updatedAddresses = addressProvider.addresses;
+    expect(updatedAddresses.length, initialCount + 1);
+    Address? updatedShippingAddress = updatedAddresses.firstWhere((p) => p.line1 == shippingAddress.line1 && p.city == shippingAddress.city);
+    // 设置刚刚添加的 shipping address 为默认 shipping address
+    await addressProvider.saveShippingAddress(updatedShippingAddress);
+    await addressProvider.fetchShippingAddress();
+
+    // 检查默认 shipping address 是否为刚刚添加的 shipping address
+    Address? defaultShippingAddress = addressProvider.shippingAddress;
+    expect(defaultShippingAddress?.id, updatedShippingAddress.id);
+
+    // 添加一个 billing address
+    Address billingAddress = Address(
+      line1: '456 Billing St.',
+      city: 'Billing City',
+      state: 'Billing State',
+      country: 'USA',
+      postalCode: '23456',
+    );
+    await addressProvider.addAddress(billingAddress);
+
+    // 检查目前 address 列表是否增加一个 item
+    await addressProvider.fetchAddresses();
+    List<Address> updatedAddresses2 = addressProvider.addresses;
+    expect(updatedAddresses2.length, initialCount + 2);
+    Address? updatedBillingAddress = updatedAddresses2.firstWhere((p) => p.line1 == billingAddress.line1 && p.city == billingAddress.city);
+    // 设置刚刚添加的 billing address 为默认 billing address
+    await addressProvider.saveBillingAddress(updatedBillingAddress);
+    await addressProvider.fetchBillingAddress();
+
+    // 检查默认 billing address 是否为刚刚添加的 billing address
+    Address? defaultBillingAddress = addressProvider.billingAddress;
+    expect(defaultBillingAddress?.id, updatedBillingAddress.id);
+
+    await addressProvider.deleteAddress(updatedShippingAddress.id);
+    await addressProvider.deleteAddress(updatedBillingAddress.id);
+    await addressProvider.fetchAddresses();
+    List<Address> endAddresses = addressProvider.addresses;
+    expect(endAddresses.length, initialCount);
   });
 }
