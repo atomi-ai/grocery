c1d8e0f3272bf2a35826ded805ad0f35e00814d8
Zephyr Guo <xiaofeng@atomi.ai>
2023-04-23 12:09:21 -0700

    跟golang后端的联调调整，确保能拿回来一个atomi.User。

--- lib/src/provider/user_provider.dart
+++ lib/src/provider/user_provider.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fryo/src/api/api_client.dart';
+import 'package:fryo/src/api/api_client.dart' as api;
 import 'package:fryo/src/shared/config.dart';
+import 'package:fryo/src/entity/user.dart' as atomi;
+
 
 class UserProvider with ChangeNotifier {
   // 如果使用User的时候因为import firebase_auth的原因导致正常测试写不了的话，
@@ -12,9 +14,13 @@ class UserProvider with ChangeNotifier {
   bool get isLoggedIn => _user != null;
 
   Future<void> login(User? user) async {
-    await get(url: "${Config.instance.apiUrl}/login");
-
-    _user = user;
+    atomi.User? atomiUser = await api.get(
+      url:'${Config.instance.apiUrl}/login',
+      fromJson: (json) => atomi.User.fromJson(json),
+    );
+    if (atomiUser != null) {
+      _user = user;
+    }
     notifyListeners();
   }
 
